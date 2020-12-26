19a31a686998a4034a0245b85e01ce3085addf83
Killian Dectot <killiandectot@gmail.com>
2020-12-27 00:27:34 +0100

    ch7

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -24,10 +24,10 @@ urlpatterns = [
     
     # User management
     
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
     
     # Local Apps
     
-    path('accounts/', include('users.urls')),
+    #path('accounts/', include('users.urls')),
     path('', include('pages.urls')), 
 ]
